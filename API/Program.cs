8d59e4643f4f6febbec345a090bd25a7fdd575d9
flaviamedici <m.olivares8915@edmail.edcc.edu>
2023-08-06 10:21:05 -0700

    Implemented global error handling in both the API and the Angular app

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,14 @@ builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
+if(builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
 // Configure the HTTP request pipeline.
+
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthentication();
